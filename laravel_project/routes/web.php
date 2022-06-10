76155eec2040927e3d15ba1c6ca69055166b94b3
AssmaIbdaiwi <100359495+AssmaIbdaiwi@users.noreply.github.com>
2022-06-10 16:01:33 +0300

    main

--- laravel_project/routes/web.php
+++ laravel_project/routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('indexx');
 });
 
+
+
+
+
+// registration
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
