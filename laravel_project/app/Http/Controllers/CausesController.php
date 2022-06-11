3947c63399e4dc9581ce9c8b266c9fafb5a3e801
MohammedAl-Amin <mohammedamin.ababneh@gmail.com>
2022-06-11 12:52:26 +0300

    main

--- laravel_project/app/Http/Controllers/CausesController.php
+++ laravel_project/app/Http/Controllers/CausesController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use LengthException;
 
 class CausesController extends Controller
 {
+
     public function index(){
-        return view("causes");
+        $products = Product::all();
+        $item_per_page = 6;
+        $pages_number = ceil(count($products) / $item_per_page);
+        $product = (array)$products;
+        $product = $product["\x00*\x00items"];
+        $pages = array();
+        for($i = 0; $i < count($products) ; $i +=$item_per_page){
+        array_push($pages, array_slice($product , $i ,$i + $item_per_page));
+        }
+        return view("causes" , compact('pages_number' , 'pages'));
     }
+    
 }
