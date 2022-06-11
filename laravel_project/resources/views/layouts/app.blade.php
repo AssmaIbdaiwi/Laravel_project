716a2d1c8db0c3f54c84de26e89d3a0e9058f797
ashrafabueraq <ashrafabueraq97@gmail.com>
2022-06-11 14:42:56 +0300

    main

--- laravel_project/resources/views/layouts/app.blade.php
+++ laravel_project/resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    
+
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
     <!-- JavaScript Bundle with Popper -->
@@ -18,10 +20,14 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    
+
     <!-- Styles -->
      <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
 </head>
 <body>
     <div id="app">
